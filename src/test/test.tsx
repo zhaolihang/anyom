400e8a23a71b101542e78d2d4699638be7f42fef
zhaolihang <625335512@qq.com>
2017-09-08 17:41:57 +0800

    测试

--- src/test/test.tsx
+++ src/test/test.tsx
@@ -15,7 +15,7 @@ let rootVNode1 = (<div>
 
 let rootRNode = createElement(rootVNode1)
 
-let rootVNode2 = (<div>
+let rootVNode2 = (<div data={{ a: 0 }}>
     <img src={'2.png'} ></img>
     <input value={'00'} ></input>
     <img src={'3.png'} ></img>
@@ -25,7 +25,8 @@ let rootVNode2 = (<div>
 let patches = diff(rootVNode1, rootVNode2);
 log(patches);
 
-assert((patches[2] as VPatch).type === VPatchType.VNODE);
+assert((patches[0] as VPatch[])[0].type === VPatchType.PROPS);
+assert((patches[0] as VPatch[])[1].type === VPatchType.INSERT);
 
 log(rootRNode);
 let newRootRNode = patch(rootRNode, patches);
