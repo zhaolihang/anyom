00a9826645e0cce64c4895b079b553146d275d09
zhaolihang <625335512@qq.com>
2017-09-19 13:40:50 +0800

    压缩文件大小

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,14 @@
 // rollup.config.js
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
+import uglify from 'rollup-plugin-uglify';
 export default {
     entry: './src/index.ts',
     format: 'umd',
     plugins: [
         resolve(),
         typescript(),
+        uglify(),
     ],
     dest: './dist/index.js',
     moduleName: 'PureVxom',
